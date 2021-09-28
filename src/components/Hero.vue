50c6bb208d5ae76b73d3e285b1d2a6e9b7899013
SamSepiool <simone.severini@outlook.it>
2021-09-28 21:35:03 +0200

    add Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img src="../assets/avengers-infinity-war-1.png" alt="">
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Hero'
+}
+</script>
+
+<style scoped>
+div{
+    width: 80%;
+    margin: auto;
+}
+
+div img{
+    width: 100%;
+}
+</style>
